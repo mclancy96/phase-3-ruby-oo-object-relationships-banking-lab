6e991805a8e57c0266d59e5c7eb2f124dd63119c
Mike <mclancy96@gmail.com>
2025-07-24 14:20:19 -0400

    bank account initialize

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,5 +8,5 @@ require_relative '../lib/transfer'
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  config.order = 'default'
+  config.order = 'defined'
 end
